4ceaa77dc2c703b7dabbba9d5ae502e59d0e9aa8
PatrikB <patrik@bjornmedia.se>
2017-12-15 15:03:15 +0100

    fixes ssl

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express   = require('express'),
-      path      = require('path'),
-      app       = express();
+const express       = require('express'),
+      path          = require('path'),
+      httpsRedirect = require('express-https-redirect'),
+      app           = express();
 
+app.use('/', httpsRedirect());
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -10,19 +12,19 @@ const title = 'ewebs – eventweb solutions';
 
 app.get('/', (req, res) => {
     
-    res.redirect('https://' + req.headers.host + req.url).render('index', { title: title, id: 'index' })
+    res.render('index', { title: title, id: 'index' })
     
 });
 
 app.get('/funktioner', (req, res) => {
     
-    res.redirect('https://' + req.headers.host + req.url).render('funktioner', { title: 'Funktioner – ' + title, id: 'funktioner' })
+    res.render('funktioner', { title: 'Funktioner – ' + title, id: 'funktioner' })
     
 });
 
 app.get('/projekt', (req, res) => {
     
-    res.redirect('https://' + req.headers.host + req.url).render('projekt', { title: 'Projekt – ' + title, id: 'projekt' })
+    res.render('projekt', { title: 'Projekt – ' + title, id: 'projekt' })
     
 });
 
@@ -34,21 +36,21 @@ app.get('/projekt/:project', (req, res) => {
         project = projects[req.params.project];
     
     if(!project)
-        res.status(404).redirect('https://' + req.headers.host + req.url).render('404');
+        res.status(404).render('404');
     else
-        res.redirect('https://' + req.headers.host + req.url).render('projekt-details', { title: project + ' – Projekt – ' + title, id: 'projekt', project: req.params.project, label: project })
+        res.render('projekt-details', { title: project + ' – Projekt – ' + title, id: 'projekt', project: req.params.project, label: project })
     
 });
 
 app.get('/kontakt', (req, res) => {
     
-    res.redirect('https://' + req.headers.host + req.url).render('kontakt', { title: 'Kontakt – ' + title, id: 'kontakt' })
+    res.render('kontakt', { title: 'Kontakt – ' + title, id: 'kontakt' })
     
 });
 
 app.get('*', (req, res) => {
    
-    res.redirect('https://' + req.headers.host + req.url).render('404', { title: '404 – ' + title, id: '404' })
+    res.render('404', { title: '404 – ' + title, id: '404' })
     
 });
 
